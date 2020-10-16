00f40ecaadc8632d4b93e201e23a3fe2d79a9a48
Crowdbotics <team@crowdbotics.com>
2020-10-16 07:21:04 +0000

    Update models

--- backend/lazy_stars_21561/wsgi.py
+++ backend/lazy_stars_21561/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lazy_stars_21561.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lazy_stars_21561.settings")
 
 application = get_wsgi_application()
